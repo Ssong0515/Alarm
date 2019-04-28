2b753eb4a930dae95433554fcc02bd3205987136
Song <406605@naver.com>
2019-04-28 20:46:47 +0900

    date picker와 timer로 알람시간을 맞추고, 알람 시간이 되면 1분동안 빨간 배경이 되게 한다 스위치 버튼은 알람을 켰을 때, 껐을 때를 나타낸다

--- Alarm/ViewController.swift
+++ Alarm/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var alarmTime: String = ""
+    
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var alarmTimeLabel: UILabel!
+    @IBOutlet weak var switchState: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(currentTime), userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func currentTime(){
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh : mm : ss"
+        let formatter3 = DateFormatter()
+        formatter3.dateFormat = "hh : mm : 00"
+        currentTimeLabel.text = "현재 시간 : " + formatter.string(from: date)
+        if switchState.isOn {
+            if formatter3.string(from: date) == alarmTime {
+                view.backgroundColor = UIColor.red
+            } else {
+                view.backgroundColor = UIColor.white
+            }
+        } else {
+            if view.backgroundColor == UIColor.red {
+                view.backgroundColor = UIColor.white
+            }
+        }
+    }
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let date = sender.date
+        let formatter2 = DateFormatter()
+        formatter2.dateFormat = "hh : mm : 00"
+        alarmTime = formatter2.string(from: date)
+        alarmTimeLabel.text = "알람 시간 : " + alarmTime
+    }
 }
-
